a253a386160c8fb3d75ef6934ffd550a8b4c352c
Chandan Mehto <mehtochandan78@gmail.com>
2025-04-09 17:52:02 +0530

    Learned Priority , Sets,Eraser

--- 03_CPP_STL/vec.cpp
+++ 03_CPP_STL/vec.cpp
@@ -168,6 +168,77 @@ void explainQueue(){
     // size swap empty same as stack
 }
 
+void explainPQ(){
+    //=> it will gives priority to the largest value elements.
+    priority_queue<int>pq;
+    pq.push(5);   //{5}
+    pq.push(2);   //{5, 2}
+    pq.push(8);   //{8, 5, 2}
+    pq.emplace(10); //{10, 8, 5, 2} 
+    
+    cout <<pq.top();  //Prints 10 because its largest element.
+
+    //=> size swap  empty function same as others
+
+    //=> minimum heap
+    priority_queue<int, vector <int>, greater<int>> pq;
+    pq.push(5); //{5}
+    pq.push(2); //{2, 5}
+    pq.push(8); //{2, 5, 8 }
+    pq.emplace(10); //{2, 5, 8, 10}
+    cout <<pq.top(); //=> it will print 2
+
+    //=> what is the time complexity of  push? : push happens in log n
+    //=> top happens of big O(1)
+    //=> : push happens in log n
+}
+
+void explainSet(){
+    //=> set: In Set everything is shorted and unique.
+
+    set<int> st;
+    st.insert(1);  //{1}
+    st.emplace(2);  //{1, 2}
+    st.insert(2);  //{1, 2}
+    st.insert(4);  //{1, 2, 4}
+    st.insert(3);  //{1, 2, 3, 4}
+    
+    //Functionality of insert in vector can be used also , that only increases efficiency .
+
+
+    //begin(), end(), rbegin(), rend(),  size(), empty() and swap are same as those of above
+
+
+    //{1, 2, 3, 4, 5}
+    //=> iterator : which point to the memory.
+    auto it = st.find(3);
+
+        //{1, 2, 3, 4, 5}
+        auto it = st.find(6); //=> it will return st.end() because 6 isnt inside the set.
+
+        //{1,4,5}
+        st.erase(5); //=> it will delete 5 and maintain shorted order. // takes logarithmic time
+
+        int cnt = st.count(1); //=> if it exist it will return 1 otherwise 0 because set contains unique value.
+
+        auto it = st.find(3);
+        st.erase(it); //=> it will will delete iterator, & it takes constant time.
+
+
+        //=> {1, 2, 3, 4, 5}
+        auto it1  = st.find(2);
+        auto it2  = st.find(4); 
+        st.erase(it1, it2);  //=> after erase: {1, 4, 5}
+        //[first, last], basically  it start erasing from 2 but it will end erasing right before 4.
+
+        //=> lower_bound() and upper_bound()  functions works in the same way as in vector  it does.
+
+        //This is the syntax=>
+        auto  it = st.lower_bound(2);
+        auto it = st.upper_bound(3);
+ 
+}
+
 int main(){
     return 0;
 }
