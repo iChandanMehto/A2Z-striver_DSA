20d57b6996f1a1648ccf0a72a59c1a1a443a5891
Chandan Mehto <mehtochandan78@gmail.com>
2025-05-20 08:11:43 +0530

    Learned Count digit - reverse Num - Palindrome Num

---
+++ 05_Basic_Maths/maths.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int n; //=> first we re declaring a int for input of  nums.
+
+cin >> n;
+int revNum = 0; //=> then we re declaring another int for reversing num.
+
+while(n>0){  //=> creating Loop of n int if its morre then 0.
+int ld = n%10;  //=> and then we re doing module so we can get the last digit.
+
+revNum = (revNum*10)+ld;  
+n = n/10;
+
+}
+cout << revNum;
+    return 0;
+}
