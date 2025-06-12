5936bd3ed59b13bd33f4dc1a519d534d0525798d
Chandan Mehto <mehtochandan78@gmail.com>
2025-06-12 21:09:58 +0530

    reverse & palindrome numberr

--- step 01-Basic/05_Basic_Maths/reverseNumber.cpp
+++ step 01-Basic/05_Basic_Maths/reverseNumber.cpp
@@ -1,29 +1,22 @@
 #include <bits/stdc++.h>
-
-//=> any number which is same on reverse is same number are called palindrom number.
-
 using namespace std;
 
-int palindromeNum(){
-    int n;
+int reverseNumber() {
+int n;
 cin >> n;
 int revNum = 0;
-int dup  = n;
 while(n>0){
     int ld = n%10; //=> here we re taking last digit of number given by user .
     revNum =( revNum*10)  + ld; //=> and then we re wrapping it in paranthysis  and multiplying * 10  and then doing additiion to ld.
 
 n = n/ 10;  
+
 }
-return dup == revNum;
+return revNum;
 }
 
-int main(){
-        bool result = palindromeNum();
-    if (result)
-        cout << "true";
-    else
-        cout << "false";
-
+int main() {
+int finaleRevNum = reverseNumber();
+cout << finaleRevNum;
     return 0;
 }
