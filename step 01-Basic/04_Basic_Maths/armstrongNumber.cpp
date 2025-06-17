47c54357658752e328f04dcab5d6a39232dc1efc
Chandan Mehto <mehtochandan78@gmail.com>
2025-06-17 17:16:12 +0530

    armstong Number

--- step 01-Basic/04_Basic_Maths/armstrongNumber.cpp
+++ step 01-Basic/04_Basic_Maths/armstrongNumber.cpp
@@ -16,8 +16,21 @@ while(n >0){
 return sum == dup;
 }
 
+int armstrongFunc(){
+    int n = 0;
+    cin >> n;
+    int dupNum = n;
+    int sumNum = 0;
+    while(n>0){
+        int lastDigit = n%10;
+        sumNum = sumNum+(lastDigit*lastDigit*lastDigit);
+        n= n/10;
+    }
+    return sumNum == dupNum;
+}
+
 int main(){
- bool finaleResult = armstrongNum();
+ bool finaleResult = armstrongFunc();
      if (finaleResult)
         cout << "true";
     else
