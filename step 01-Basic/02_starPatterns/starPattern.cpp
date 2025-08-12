a8a9ff0d4f9bfdf98ac2db6f4a3829d8115e5efe
Chandan Mehto <mehtochandan78@gmail.com>
2025-08-12 15:59:53 +0530

    04_Palindrome in recursion

--- step 01-Basic/02_starPatterns/starPattern.cpp
+++ step 01-Basic/02_starPatterns/starPattern.cpp
@@ -2,17 +2,43 @@
 
 using namespace std;
 
+
+// Print Pattern 5*3
+
 int firstPattern(){
     int i, j;
     for(int i = 1; i <= 5; i++){
         for(int j = 1; j<=5; j++){
-            cout << "*"<< endl;
+    if(j<= i){
+                cout << "* ";
+    } else{
+        cout << " ";
+    }
         }
+        cout << "\n";
     }
 }
 
+
+int secPattern(){
+    int i, j;
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j<=5; j++){
+    if(j>=6-i){
+                cout << "*";
+    } else{
+        cout << " ";
+    }
+        }
+        cout << "\n";
+    }
+}
+
+
+
+
 int main(){
-    return 0;
-    firstPattern();
-    cout << firstPattern()<< endl;/
+
+    secPattern();
+        return 0;
 }
--- step 01-Basic/05_Basic_Recursion/03_functionalRec.cpp
+++ step 01-Basic/05_Basic_Recursion/03_functionalRec.cpp
@@ -11,13 +11,27 @@ void revArr(int i, int arr[],  int n){
 
 }
 
-int main(){
-int n;    
-cin >> n;
-int arr[n];
-for(int i = 0; i<n; i++) cin >> arr[i];
-    revArr(0, arr,n);
-for(int  i = 0;  i <n; i++) cout << arr[i] << " ";
-
-return 0;
+// Check palindrome using recursion
+bool func(int i, string &s) {
+    if (i >= s.size() / 2) return true; // base case
+    if (s[i] != s[s.size() - i - 1]) return false; // mismatch case
+    return func(i + 1, s); // recursive case
 }
+
+
+int main(){
+    string s   = "madam";
+    cout << func(0, s);
+    return 0;
+}
+
+// int main(){
+// int n;    
+// cin >> n
+// int arr[n];
+// for(int i = 0; i<n; i++) cin >> arr[i];
+//     revArr(0, arr,n);
+// for(int  i = 0;  i <n; i++) cout << arr[i] << " ";
+
+// return 0;
+// }
